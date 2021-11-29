f06b56cf26e3025256044bad5acb914b5cc05a44
sarojvrc <saroj.vrc@gmail.com>
2021-11-29 17:25:28 +0530

    navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,9 +5,9 @@ import { Search } from './Search';
 export const Navbar = ({darkTheme, setDarkTheme}) => {
     return (
         <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
-            <div className="flex justify-between items-center space-x-5 w-screen">
+            <div className="flex justify-between items-center space-x-5 w-screen ">
                 <Link to="/">
-                    <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
+                    <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
                         Google
                     </p>
                 </Link>
