90db04568abb4c02464f23d41e8c4f7ade963e0e
sarojvrc <saroj.vrc@gmail.com>
2021-11-29 15:23:57 +0530

    app updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import {Navbar} from './Components/Navbar';
+import { Footer } from './Components/Footer';
+import { Routes } from './Components/Routes';
 
 function App() {
+
+    const [darkTheme, setDarkTheme] = useState(false);
+
     return (
-        <div>
-            <h1>App</h1>
+        <div className={darkTheme ? 'dark' : ''}>
+           <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+               <Navbar />
+               <Routes />
+               <Footer />
+           </div>
         </div>
     )
 }
